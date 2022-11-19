06913c2dbcec1274d85d1d7d317298c880a060a6
Junaid Alam <102413407+Junaid2102@users.noreply.github.com>
2022-11-19 20:12:47 +0500

    HomeWork 03

---
+++ HomeWork03/DTO/GPADTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class GPADTO
+    {
+        public string? Name { get; set; }
+        public string? RollNumber { get; set; }
+        public string? Subject { get; set; }
+        public int Marks { get; set; }
+        public string? GPA { get; set; }
+        public string? Grade { get; set; }
+
+    }
+}
