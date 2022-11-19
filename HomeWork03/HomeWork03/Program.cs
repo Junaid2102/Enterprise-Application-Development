c4314117a72231e0f7ac7fad5ad0dfd90f6ed226
Junaid Alam <102413407+Junaid2102@users.noreply.github.com>
2022-11-19 20:14:58 +0500

    HomeWork 03

---
+++ HomeWork03/HomeWork03/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PL;
+
+int input;
+do
+{
+    Console.WriteLine("\nWelcome to GPA Calculator");
+    Console.WriteLine("1---Enter Student Data");
+    Console.WriteLine("2---Output Student Data");
+    Console.WriteLine("3---Exit");
+    Console.WriteLine("\nPlease choose one option from above: ");
+    input = Convert.ToInt32(Console.ReadLine());
+
+    if (input == 1)
+    {
+        GPAview view = new GPAview();
+        view.inputdetails();
+    }
+    if (input == 2)
+    {
+        GPAview view = new GPAview();
+        view.ShowData();
+        Console.WriteLine("Press any key to go to main menu……...");
+        Console.ReadKey(true);
+    }
+} while (input != 3);
